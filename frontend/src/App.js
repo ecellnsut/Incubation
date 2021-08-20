660d8a70e081df9fe7f6ab95c95b2b490e49a5c5
Gaurav Bansal <bansalgaurav726@gmail.com>
2021-08-20 14:48:06 +0530

    Added stats component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React, { Fragment } from 'react';
 import Navbar from './components/Navbar';
 import Landing from './components/Landing';
+import Stats from './components/Stats';
+import Services from './components/Services';
 import './App.css';
 
 const App = () => {
@@ -8,6 +10,8 @@ const App = () => {
     <Fragment>
       <Navbar />
       <Landing />
+      <Stats />
+      <Services />
     </Fragment>
   );
 };
---
+++ frontend/src/components/Stats.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Stats = () => {
+  return (
+    <div id="stats">
+      <div className="stats-bg"></div>
+      <div className="stats-cards">
+        <div className="stats-card">
+          <i className="fas fa-tasks fa-3x"></i>
+          <h2>500+</h2>
+          <p className="lead">lorem ipsum</p>
+        </div>
+        <div className="stats-card">
+          <i className="fas fa-file-alt fa-3x"></i>
+          <h2>#10</h2>
+          <p className="lead">lorem ipsum</p>
+        </div>
+        <div className="stats-card">
+          <i className="fas fa-shopping-bag fa-3x"></i>
+          <h2>$500k</h2>
+          <p className="lead">lorem ipsum</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Stats;
