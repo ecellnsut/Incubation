d96648a86abaeaa2656a3af0138d71956b020269
Gaurav Bansal <bansalgaurav726@gmail.com>
2021-08-20 16:24:56 +0530

    Added Services Component

--- frontend/src/components/Services.js
+++ frontend/src/components/Services.js
@@ -1,7 +1,64 @@
 import React from 'react';
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div id="services">
+      <div className="container">
+        <div className="service">
+          <div className="service-text">
+            <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Accusantium, quisquam illo libero quod ut qui quia, explicabo
+              consectetur iusto porro modi rerum incidunt. Alias cumque minima
+              dolores voluptate! Tempore impedit ducimus voluptatibus similique
+              quasi ex!
+            </p>
+          </div>
+          <div className="service-img service-1"></div>
+        </div>
+        <div className="service">
+          <div className="service-img service-2"></div>
+          <div className="service-text">
+            <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Accusantium, quisquam illo libero quod ut qui quia, explicabo
+              consectetur iusto porro modi rerum incidunt. Alias cumque minima
+              dolores voluptate! Tempore impedit ducimus voluptatibus similique
+              quasi ex!
+            </p>
+          </div>
+        </div>
+        <div className="service">
+          <div className="service-text">
+            <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Accusantium, quisquam illo libero quod ut qui quia, explicabo
+              consectetur iusto porro modi rerum incidunt. Alias cumque minima
+              dolores voluptate! Tempore impedit ducimus voluptatibus similique
+              quasi ex!
+            </p>
+          </div>
+          <div className="service-img service-3"></div>
+        </div>
+        <div className="service">
+          <div className="service-img service-4"></div>
+          <div className="service-text">
+            <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Accusantium, quisquam illo libero quod ut qui quia, explicabo
+              consectetur iusto porro modi rerum incidunt. Alias cumque minima
+              dolores voluptate! Tempore impedit ducimus voluptatibus similique
+              quasi ex!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Services;
